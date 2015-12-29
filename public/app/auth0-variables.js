b469366e986bcce2a14fd35eb513bf5f6b619368
YenHuang <yenhuang@bellsouth.net>
2015-12-29 17:24:57 -0500

    hooking in angular

---
+++ public/app/auth0-variables.js
@@ -0,0 +1,3 @@
+var AUTH0_CLIENT_ID='1nfRfyTY2xtWLXoF1D3Q2JfsjZp7ZYxk'; 
+var AUTH0_DOMAIN='yenhuang.auth0.com'; 
+var AUTH0_CALLBACK_URL=location.href;
---
+++ public/app/home/home.js
@@ -0,0 +1,19 @@
+angular.module( 'Pearson.home', [
+'auth0'
+])
+.controller( 'HomeCtrl', function HomeController( $scope, auth, $http, $location, store ) {
+
+  $scope.auth = auth;
+
+  $scope.logout = function() {
+    auth.signout();
+    store.remove('profile');
+    store.remove('token');
+    $location.path('/login');
+  }
+
+  
+
+  
+
+});
