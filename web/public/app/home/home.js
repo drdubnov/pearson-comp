cb87ae135e96ea3f3d9815b294672e2c1ba24c93
YenHuang <yenhuang@bellsouth.net>
2015-12-29 17:40:55 -0500

    hooked in node

--- web/public/app/home/home.js
+++ web/public/app/home/home.js
@@ -13,7 +13,19 @@ angular.module( 'Pearson.home', [
   }
 
   
-
+   $http({
+      url: 'http://localhost:3001/secured/ping',
+      method: 'GET'
+    }).then(function() {
+      alert("We got the secured data successfully");
+    }, function(response) {
+      if (response.status == 0) {
+        alert("Please download the API seed so that you can call it.");
+      }
+      else {
+        alert(response.data);
+      }
+    });
   
 
 });
