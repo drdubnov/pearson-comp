411b3f3bb1d38dfe427cb2e97589d50e21679cfa
Jorghi12 <jd728@cornell.edu>
2015-12-29 22:17:51 -0600

    Added Search Results

--- web/public/app/home/home.js
+++ web/public/app/home/home.js
@@ -14,7 +14,7 @@ angular.module( 'Pearson.home', [
 
   
    
-
+	$scope.search = function(){
     $http({
       url: 'http://localhost:3001/secured/searchFTArticles',
       method: 'GET',
@@ -22,10 +22,15 @@ angular.module( 'Pearson.home', [
       	search: "Economy"
       }
     }).then(function(response) {
+		window.legend = response["data"]["articles"]["results"][0];
+		document.getElementById("results").innerHTML = (response["data"]["articles"]["count"] == 0) ? "No results yet" : "";
+		for (var i =0;i<response["data"]["articles"]["count"];i++){
+			new_link = '<a href = "' + response["data"]["articles"]["results"][i]["article_url"] + '">' + response["data"]["articles"]["results"][i]["headline"] + '</a>';
+			document.getElementById("results").innerHTML += new_link + "<br><br>";
+		}
         console.log(response["data"]["articles"]);
     }); // end of http get
-
-
+	}
 
   
 
