f27fef0f552260b13bc144c98d80c054fbe99a2d
Jorghi12 <jd728@cornell.edu>
2015-12-31 17:55:16 -0600

    Some updates
    Need to get the green arrows (scroll buttons) moved around.

--- web/public/app/home/home.js
+++ web/public/app/home/home.js
@@ -72,6 +72,35 @@ angular.module( 'Pearson.home', [
   
   window.grabText = $scope.grabText;
 
+$('#search').on("keypress", function(e) {
+        if (e.keyCode == 13) {
+			$scope.search();
+            return false; // prevent the button click from happening
+        }
+});
+
+textScrollUp = function(){
+	document.getElementById("infoarea").scrollTop +=300
+}
+
+textScrollDown = function(){
+	document.getElementById("infoarea").scrollTop -=300
+}
+
+var copyTextareaBtn = document.querySelector('#copyButton');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var copyTextarea = document.querySelector('#typearea');
+  copyTextarea.select();
+	
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+});
 
 $scope.search = function(){
 	$http({
