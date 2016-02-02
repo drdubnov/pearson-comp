f9186982b79244c0ddda15e26ede62be87d1655d
Nikita Dubnov <nikita.dubnov@gmail.com>
2016-02-01 22:11:02 -0500

    style updates

--- web/public/app/app.js
+++ web/public/app/app.js
@@ -54,7 +54,7 @@ angular.module( 'Pearson', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$routeChangeSuccess', function(e, nextRoute){
     if ( nextRoute.$$route && angular.isDefined( nextRoute.$$route.pageTitle ) ) {
-      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Pearson' ;
+      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Mint App' ;
     }
   });
 })
